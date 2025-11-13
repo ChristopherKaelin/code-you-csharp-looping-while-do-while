4c3ffab21f0f15464365f14275cf8866a04fc5d3
Chris Kaelin (EagleUK) <chriskaelin67@gmail.com>
2025-11-13 16:59:04 -0500

    MS Learn Challenge Projects

--- Program.cs
+++ Program.cs
@@ -1,35 +1,59 @@
-Console.WriteLine("\n====================\n");
+Console.WriteLine("\n====================");
+Console.WriteLine("==   Project 1  ==");
 
-Random dice = new Random();
+string? readResult;
+int numericValue = 0;
+bool validNumber = false;
 
-int monsterHealth = 10;
-int heroHealth = 10;
+while (!validNumber) {
+    Console.WriteLine("Enter a number:");
+    readResult = Console.ReadLine();
 
-Console.WriteLine($"Monster Health: {monsterHealth}");
-Console.WriteLine($"Hero Health: {heroHealth}");
-
-do
-{
-    int heroAttack = dice.Next(1, 10);
-    monsterHealth -= heroAttack;
-    Console.WriteLine($"\nHero attacks.  Monster was damaged and lost {heroAttack} health and now has {monsterHealth} health");
-
-    if (monsterHealth <= 0)
-    {
-        Console.WriteLine("Hero wins!");
-        continue;
+    validNumber = int.TryParse(readResult, out numericValue);
+    if (!validNumber) {
+        Console.WriteLine($"{readResult} is NOT a valid number.  Try Again");
+    } else if (numericValue <5 || numericValue >10)
+        {Console.WriteLine($"{numericValue} is a valid number, but not between 5 and 10.  Try Again.");
+        validNumber = false;
     }
+}
+Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
 
-    int monsterAttack = dice.Next(1, 10);
-    heroHealth -= monsterAttack;
-    Console.WriteLine($"Monster attacks.  Hero was damaged and lost {monsterAttack} health and now has {heroHealth} health");
 
-    if (heroHealth <= 0)
-    {
-        Console.WriteLine("Monster wins!");
-        continue;
+Console.WriteLine("\n====================");
+Console.WriteLine("==   Project 2  ==");
+
+string readRole = "";
+string compareRole = "";
+bool validRole = false;
+
+while (!validRole) {
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User");
+    readRole = Console.ReadLine();
+    compareRole = readRole.Trim().ToLower();
+    if (compareRole == "administrator" || compareRole ==  "manager" || compareRole == "user") {
+        validRole =  true;
+    } else {
+        Console.WriteLine($"The role name that you entered, '{readRole}' is not valid. ");
     }
-} while (monsterHealth > 0 && heroHealth > 0);
+}
+Console.WriteLine($"Your input value ({readRole}) has been accepted.");
 
 
+Console.WriteLine("\n====================");
+Console.WriteLine("==   Project 3  ==");
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+string sentence = "";
+int periodLocation = 0;
+
+foreach(string stringText in myStrings){
+    sentence = stringText;
+    periodLocation = sentence.IndexOf(".");
+    while (periodLocation > 0){
+        Console.WriteLine($"{sentence.Substring(0,periodLocation)}");
+        sentence = sentence.Remove(0,periodLocation+1).TrimStart();
+        periodLocation = sentence.IndexOf(".");
+    }
+    Console.WriteLine($"{sentence}");
+}
 Console.WriteLine("\n====================\n");
